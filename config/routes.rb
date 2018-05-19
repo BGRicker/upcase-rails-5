653a3ba28ab78d55d8e11a4b11d3279c7ec08df4
Benjamin Ricker <benjaminricker@Benjamins-iMac.local>
2018-05-19 17:36:05 -0400

    allow user to add shouts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   end
 
   root "homes#show"
+
+  resources :shouts, only: [:create]
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
